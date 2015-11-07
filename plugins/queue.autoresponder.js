89302f3f82ebbc53e54ccceed4149b5c95939ba1
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2015-11-07 01:16:30 +0100

    Don't respond twice & deny non-users

--- plugins/queue.autoresponder.js
+++ plugins/queue.autoresponder.js
@@ -7,63 +7,68 @@
 
 var outbound = require('./outbound');
 var mailcomposer = require('mailcomposer');
+var FifoArray = require('fifo-array');
+var respondedTo = new FifoArray(1000);
 
-exports.hook_queue = function (next, connection) {
-	var userMap = this.config.get('queue.autoresponder.json', 'json') || {};
-	
-    // basic logging so we can see if we have an email hitting the stack
-    this.loginfo("New inbound email detected, handling");
+exports.hook_queue = function(next, connection) {
+  var userMap = this.config.get('queue.autoresponder.json', 'json') || {};
 
-    // setup of variable to save us typing the same string over and over.
-    var transaction = connection.transaction;
-    var receivedDate = transaction.header.headers.date;
-    var subjectLine = transaction.header.headers.subject;
+  // basic logging so we can see if we have an email hitting the stack
+  this.loginfo("New inbound email detected, handling");
 
-    this.loginfo({
-        email: transaction.mail_from,
-        message: transaction.data_lines,
-        received: receivedDate,
-        subjectLine: subjectLine
-	});
+  // setup of variable to save us typing the same string over and over.
+  var transaction = connection.transaction;
+  var receivedDate = transaction.header.headers.date;
+  var subjectLine = transaction.header.headers.subject;
+/*
+  this.loginfo({
+    email: transaction.mail_from,
+    message: transaction.data_lines,
+    received: receivedDate,
+    subjectLine: subjectLine
+  });
+*/
+  if (respondedTo.indexOf(transaction.mail_from.original) > -1) {
+    return next(DENY, 'Too many emails, too soon!');
+  }
+  if (transaction.rcpt_to.length > 0 &&
+			userMap.hasOwnProperty(transaction.rcpt_to[0].user) &&
+			transaction.rcpt_to[0].original != transaction.mail_from.original) {
 
-	if (transaction.rcpt_to.length > 0 && userMap.hasOwnProperty(transaction.rcpt_to[0].user) && transaction.rcpt_to[0].user != transaction.mail_from.original) {
-		var mailObj = userMap[transaction.rcpt_to[0].user];
+		respondedTo.push(transaction.mail_from.original);
 
-		if (!mailObj.hasOwnProperty('from'))
-			mailObj.from = transaction.rcpt_to[0].original;
+    var mailObj = userMap[transaction.rcpt_to[0].user];
 
-		mailObj.to = transaction.mail_from.original;
-		var messageIds = connection.transaction.header.get_all('Message-Id');
-		if (messageIds.length > 0)
-			mailObj.inReplyTo = messageIds[0];
+    if (!mailObj.hasOwnProperty('from'))
+      mailObj.from = transaction.rcpt_to[0].original;
 
-		var mail = mailcomposer(mailObj);
+    mailObj.to = transaction.mail_from.original;
+    var messageIds = connection.transaction.header.get_all('Message-Id');
+    if (messageIds.length > 0)
+      mailObj.inReplyTo = messageIds[0];
 
-		var outnext = (code, msg) => {
-			switch (code) {
-				case DENY: this.logerror("Sending mail failed: " + msg);
-                    break;
-				case OK: this.loginfo("Autoreply sent");
-                    next();
-                    break;
-				default: this.logerror("Unrecognised return code from sending email: " + msg);
-                    next();
+    var mail = mailcomposer(mailObj);
+
+    mail.build((err, message) => {
+      if (!err) {
+        // this.loginfo(mailObj);
+        // this.loginfo(message.toString());
+        this.loginfo('Sending auto-response');
+        outbound.send_email(mailObj.from, mailObj.to, message.toString(), (code, msg) => {
+		      switch (code) {
+		        case DENY: this.logerror("Sending mail failed: " + msg); return next(DENY); break;
+		        case OK: this.loginfo("Autoreply sent"); return next(OK); break;
+		        default: this.logerror("Unrecognised return code from sending email: " + msg); return next(DENY);
+		      }
+		    });
+      }
+      else {
+				this.logerror('Unable to generate auto-response: ' + err);
+				return next(DENY, "Something strange has happened.");
 			}
-		};
-
-		mail.build((err, message) => {
-			if (!err) {
-				this.loginfo(mailObj);
-				// this.loginfo(message.toString());
-				this.loginfo('Sending auto-response');
-				outbound.send_email(mailObj.from, mailObj.to, message.toString(), outnext);
-			}
-			else this.logerror('Unable to generate auto-response: ' + err);
-			next();
-		});
-	}
-	else {
-		// passes control over to the next plugin within Haraka.
-		next(OK);
-	}
+    });
+  } else {
+    // passes control over to the next plugin within Haraka.
+    return next();
+  }
 }
